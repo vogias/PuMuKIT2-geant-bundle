2940e6c09c0fc904c526112d54c172f80d472df7
Alfro <arodriguez@teltek.es>
2016-02-25 11:29:18 +0100

    Restored version text back to 'dev'.

--- PumukitGeantWebTVBundle.php
+++ PumukitGeantWebTVBundle.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PumukitGeantWebTVBundle extends Bundle
 {
-  const VERSION = '1.0.1-RC1';
+  const VERSION = '1.0.1-dev';
   public function getParent()
   {
     return 'PumukitWebTVBundle';
