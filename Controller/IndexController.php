144b2406b155b21267a49bf453a279e722746035
Alfro <arodriguez@teltek.es>
2016-02-26 18:24:33 +0100

    #83 Controller: Using service 'getLast'

--- Controller/IndexController.php
+++ Controller/IndexController.php
@@ -15,23 +15,14 @@ class IndexController extends ParentController
      */
     public function recentlyaddedAction()
     {
-        $limit = 3;
-        if ($this->container->hasParameter('limit_objs_recentlyadded')){
-            $limit = $this->container->getParameter('limit_objs_recentlyadded');
-        }
-        $templateTitle = 'Latest Uploads';
-        if($this->container->hasParameter('menu.announces_title')) {
-            $templateTitle = $this->container->getParameter('menu.announces_title');
-        }
+        $limit = $this->container->getParameter('limit_objs_recentlyadded');
+        $numberCols = $this->container->getParameter('columns_objs_announces');
+        $templateTitle = $this->container->getParameter('menu.announces_title');
+
         $this->get('pumukit_web_tv.breadcrumbs')->addList($templateTitle, 'pumukit_webtv_announces_latestuploads');
 
         $announcesService = $this->get('pumukitschema.announce');
-
-        $lastMms = array();
-        $mmobjRepo = $this->getDoctrine()->getRepository('PumukitSchemaBundle:MultimediaObject');
-        //Get last objects without errors.
-        $lastMms = $mmobjRepo->findStandardBy(array(), array('public_date' => -1), $limit, 0);
-        $numberCols = $this->container->getParameter('columns_objs_announces');
+        $lastMms = $announcesService->getLast($limit);
 
         return array('template_title' => $templateTitle,
                      'last' => $lastMms,
