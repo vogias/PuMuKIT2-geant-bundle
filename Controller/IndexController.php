868a8106cda8b8cebb2f0677374b4471f5d4066a
Alfro <arodriguez@teltek.es>
2016-02-26 17:53:39 +0100

    #83 Fixed 'Latest Uploads/Recently added' bug.
    * Now both views show the same results.

--- Controller/IndexController.php
+++ Controller/IndexController.php
@@ -2,23 +2,23 @@
 
 namespace Pumukit\Geant\WebTVBundle\Controller;
 
-use Pumukit\SchemaBundle\Document\MultimediaObject;
-use Pumukit\SchemaBundle\Document\Series;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+use Pumukit\WebTVBundle\Controller\IndexController as ParentController;
 
-class AnnouncesController extends Controller
+class IndexController extends ParentController
 {
     /**
-     * @Route("/latestuploads", name="pumukit_webtv_announces_latestuploads")
      * @Template()
      */
-    public function latestUploadsAction(Request $request)
+    public function recentlyaddedAction()
     {
-        $limit = 20;
+        $limit = 3;
+        if ($this->container->hasParameter('limit_objs_recentlyadded')){
+            $limit = $this->container->getParameter('limit_objs_recentlyadded');
+        }
         $templateTitle = 'Latest Uploads';
         if($this->container->hasParameter('menu.announces_title')) {
             $templateTitle = $this->container->getParameter('menu.announces_title');
