f47bcb209251f2ef9a78c01ea0587c45ee707fa0
Alfro <arodriguez@teltek.es>
2016-03-04 09:23:14 +0100

    Prepared next version dev tag

--- PumukitGeantWebTVBundle.php
+++ PumukitGeantWebTVBundle.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PumukitGeantWebTVBundle extends Bundle
 {
-  const VERSION = '1.0.1';
+  const VERSION = '1.0.2-dev';
   public function getParent()
   {
     return 'PumukitWebTVBundle';
